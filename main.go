e4302fa8b968b422a3bdbf8ca161187325844396
badblli <bsnradblli@gmail.com>
2022-03-30 18:25:57 +0300

    i can go

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	var metin string = "Hello 世界!"
+	fmt.Println(metin)
 	fmt.Print("i can go ")
 	fmt.Println("World")
 	fmt.Println("Hello 世界!")
+
+	var a int = 10
+	fmt.Println(a)
 }
